f3a84c1b922410c506ea48189d4ba31c88fada83
Baptiste <113362872+Tchoup7790@users.noreply.github.com>
2026-01-07 16:43:47 +0100

    Ajout des composants au premier endpoint

--- src/views/drumcorpbsn-view.vue
+++ src/views/drumcorpbsn-view.vue
@@ -1,3 +1,14 @@
 <template>
-  <h1>Hello World!</h1>
+  <main>
+    <profile-section avatar-url="" name="DRUMCORPBSN" />
+    <link-grid :links="drumcorpbsnData as LinkData[]" />
+  </main>
 </template>
+
+<script lang="ts" setup>
+import profileSection from '@/components/profile-section.vue'
+import linkGrid from '@/components/link-grid.vue'
+
+import drumcorpbsnData from '@/assets/data/drumcorpbsn-data.json'
+import type { LinkData } from '@/models/interfaces/link-data.interface'
+</script>
