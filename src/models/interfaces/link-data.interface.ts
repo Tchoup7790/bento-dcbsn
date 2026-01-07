b5537e90a904caf321630165c67e4b12fb4ee3be
Baptiste <113362872+Tchoup7790@users.noreply.github.com>
2026-01-07 16:43:07 +0100

    Ajout d'id et host à l'interface linkData

--- src/models/interfaces/link-data.interface.ts
+++ src/models/interfaces/link-data.interface.ts
@@ -2,8 +2,10 @@ import type { CardSize } from '../enums/card-size.enum'
 import type { IconType } from '../enums/icon-type.enum'
 
 export interface LinkData {
+  id: number
   title: string
   url: string
+  host: string
   iconType: IconType
   imageUrl: string
   cardSize: CardSize
